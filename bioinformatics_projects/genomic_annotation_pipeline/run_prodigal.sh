459321950e364527469281d8d06012950015f8de
Marina Rice <mrice183@gmail.com>
2026-05-31 21:34:03 -0700

    Delete bioinformatics_projects directory

--- bioinformatics_projects/genomic_annotation_pipeline/run_prodigal.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Script to run gene prediction using Prodigal
-# -i: input sequence (FASTA)
-# -o: output annotation (GFF)
-# -f: specify GFF output format
-
-prodigal -i data/unit2_new_genome.fasta -o data/unit2_new_genome.prodigal.gff -f gff
-
-echo "Gene prediction complete. Output saved to data/unit2_new_genome.prodigal.gff"
